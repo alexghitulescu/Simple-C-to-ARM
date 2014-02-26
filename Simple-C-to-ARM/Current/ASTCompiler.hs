e8184aefdb492565cb34e1c2793009c4af526dd6
Alexandru Ghitulescu <alex.ghitulescu@gmail.com>
2014-02-26 12:25:09 +0000

    Stopped Version 0.2
    - functions without arguments
    - working in Current now

---
+++ Simple-C-to-ARM/Current/ASTCompiler.hs
@@ -0,0 +1,82 @@
+
+module ASTCompiler (
+     comp
+) where
+
+import Prelude hiding (EQ)
+import AST
+import VMInst
+import qualified Data.Map as Map
+
+comp                            :: Prog -> Code
+comp p                          = fst $ runState (compProg p) (0, Map.empty)
+
+-- State Monad 
+-- ===========
+
+-- Declaration for the state monad and a new type runState to save writing State -(a, State).
+
+data ST a     = S { runState :: State -> (a, State) }
+type State    = (Integer, Map.Map Name Pos)
+
+apply         :: ST a -> State -> (a,State)
+apply (S f)  = f 
+
+instance Monad ST where
+      -- return :: a -> ST a
+      return x   = S (\s -> (x,s))
+
+      -- (>>=)  :: ST a -> (a -> ST b) -> ST b
+      st >>= f   = S (\s -> let (x,s') = apply st s in apply (f x) s')
+
+-- The function that generates the fresh labels. It is of type ST showing that it has a hidden state. 
+
+fresh         :: ST Integer
+fresh         =  S (\(n, h) -> (n, (n+1, h)))
+
+newVars        :: [Name] -> Code
+newVars []     = []
+newVars (n:ns) = (ADDRESS n):(newVars ns)
+
+compProg                        :: Prog -> ST Code
+compProg (GlobalVar n)          = return [ADDRESS n]
+compProg (Fun n ns st)          = do code <- (compStmt st)
+                                     return ([LABELS n, PUSHR LR] ++ code ++ [BR])
+compProg (PSeq [    ])          = return []
+compProg (PSeq (x:xs))          = do code <- compProg x
+                                     code' <- compProg (PSeq xs)
+                                     return (code ++ code')
+                
+compStmt                        :: Stmt -> ST Code
+compStmt (LocalVar n)           = return [ADDRESS n]
+compStmt (Assign v e)           = return ((compExpr e) ++ [POP v])
+compStmt (Print e)              = return ((compExpr e) ++ [PRINT])
+compStmt (Seqn [    ])          = return []
+compStmt (Seqn (x:xs))          = do code <- compStmt x
+                                     code' <- compStmt (Seqn xs)
+                                     return (code ++ code')
+compStmt (While e p)            = do lb <- fresh
+                                     code <- compStmt p
+                                     lb' <- fresh
+                                     return ([LABEL lb] ++ (compExpr e) ++ (jumpz lb') ++ code ++ [BX NONE lb, LABEL (lb')])
+compStmt (If e p1 (Seqn []))    = do lb <- fresh
+                                     code <- compStmt p1
+                                     return ((compExpr e) ++ (jumpz lb) ++ code ++ [LABEL lb])
+compStmt (If e p1 p2)           = do lb <- fresh
+                                     lb' <- fresh
+                                     code <- compStmt p1
+                                     code' <- compStmt p2
+                                     return ((compExpr e) ++ (jumpz lb) ++ code ++ [BX NONE lb', LABEL lb] ++ code' ++ [LABEL (lb')])
+compStmt (Apply n e)            = return ((compExprs e) ++ [BL NONE n])
+
+jumpz                           :: Label -> Code
+jumpz lb                        = [PUSH 0, CMPST, BX EQ lb]
+                                     
+compExprs                     :: [Expr] -> Code
+compExprs []                  = []
+compExprs (x:xs)              = (compExpr x) ++ (compExprs xs)
+                                      
+compExpr                      :: Expr -> Code
+compExpr (Val n)              = [PUSH n]
+compExpr (Var v)              = [PUSHV v]
+compExpr (App op e1 e2)       = (compExpr e1) ++ (compExpr e2) ++ [DO op]
---
+++ Simple-C-to-ARM/Current/VMInst.hs
@@ -0,0 +1,59 @@
+
+module VMInst (
+    Stack  (..),
+    Mem    (..),
+    Code   (..),
+    Inst   (..),
+    Label  (..),
+    Reg    (..),
+    Cond   (..),
+    CFlag  (..),
+    Pos    (..)
+) where
+
+import AST
+import Data.Array.IArray
+
+-- Virtual machine
+-- ===============
+
+type Stack            =  Array Integer
+
+type Mem              =  [(Name, Integer)]
+
+type Code             =  [Inst]
+
+data Inst             =  ADDRESS Name
+                      |  PUSH Integer
+                      |  PUSHV Name
+                      |  POP Name
+                      |  DO Op
+                      |  CMP Reg Reg
+                      |  CMPV Reg Integer
+                      |  BX Cond Label
+                      |  BXL Cond Label
+                      |  B Cond Name
+                      |  BL Cond Name
+                      |  LABEL Label
+                      |  LABELS Name
+                      |  PRINT
+                      |  HALT
+                      |  LDR Reg Pos
+                      |  LDRV Reg Integer
+                      |  STR Reg Pos
+                      |  CMPST
+                      |  PUSHR Reg
+                      |  BR
+                         deriving (Show, Eq)
+
+data Reg              = SB | PC | LR | SP | R Name deriving (Show, Eq)
+
+data Pos              = P Reg Displacement deriving (Show, Eq)
+
+data Cond             = EQ | NE | GT | LT | GE | LE | NONE deriving (Show, Eq)
+
+data CFlag            = EQ' | GT' | LT' | NONE' deriving (Show, Eq)
+                         
+type Label            =  Integer
+
+type Displacement     =  Integer
